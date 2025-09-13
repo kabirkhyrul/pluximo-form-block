8d46282f99fa5810053fbea1b56a417457465545
Kabir Khyrul <kabir.signup@gmail.com>
2025-09-13 19:09:43 +0600

    init

---
+++ build/form-builder-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '49a298b4ccb74280535c');
---
+++ build/form-builder-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'cde223ad1003f744685a');
